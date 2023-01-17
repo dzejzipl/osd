e04632363dc09675b73dac2ca84d96860227ca12
Jakub Piesik <jakubpiesik@outlook.com>
2023-01-17 15:32:45 +0100

    Update OSD.ps1
    new script

--- OSD.ps1
+++ OSD.ps1
@@ -1,24 +1,8 @@
-Write-Host  -ForegroundColor Cyan "Modern Management Device"
-Start-Sleep -Seconds 5
+write-host -ForegroundColor Green "Startowanie z OSDCloud"
 
-#Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
+install-module OSD -Force
+import-module OSD -FOrce
 
-#Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
-Install-Module OSD -Force
-
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
-Import-Module OSD -Force
-
-#Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
 Start-OSDCloud -OSLanguage pl-PL -OSBuild 22H2 -OSEdition Pro -ZTI
 
-#Restart from WinPE
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
 wpeutil reboot
