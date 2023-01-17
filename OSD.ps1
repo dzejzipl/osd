823622eea4ba2606e4d382a75c546e5fb8c7e14b
Jakub Piesik <jakubpiesik@outlook.com>
2023-01-17 19:07:52 +0100

    Update OSD.ps1
    Some changes

--- OSD.ps1
+++ OSD.ps1
@@ -4,6 +4,9 @@ Set-ExecutionPolicy bypass
 install-module OSD -Force
 import-module OSD -FOrce
 
+write-host -ForegroundColor Green "Start OSDCloud"
 Start-OSDCloud -OSLanguage pl-PL -OSBuild 22H2 -OSVersion 'Windows 10' -OSEdition Pro -OSLicense Retail -ZTI
+
+
 
 wpeutil reboot
