d2da4f5efe557bd1b37b553c2de9ab9f1f885d98
Jakub Piesik <jakubpiesik@outlook.com>
2023-06-09 06:57:52 +0200

    Update OSD_2.ps1

--- OSD_2.ps1
+++ OSD_2.ps1
@@ -5,6 +5,6 @@ install-module OSD -Force -SkipPublisherCheck
 import-module OSD -Force
 
 write-host -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -FindImageFile -OSImageIndex 1 -ZTI
+Start-OSDCloud -FindImageFile -ZTI
 
 wpeutil reboot
