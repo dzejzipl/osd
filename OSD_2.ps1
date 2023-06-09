554ba23b2cb4de7b1dde3b414936487b2127288f
Jakub Piesik <jakubpiesik@outlook.com>
2023-06-09 10:25:30 +0200

    Update OSD_2.ps1

--- OSD_2.ps1
+++ OSD_2.ps1
@@ -1,10 +1,12 @@
-write-host -ForegroundColor Green "Startowanie z OSDCloud"
+write-host -ForegroundColor Green "Modern Management Deployment...."
+write-host -ForegroundColor Green "Startowanie z instalacja nowego oprogramowania"
 Set-ExecutionPolicy bypass
 
 install-module OSD -Force -SkipPublisherCheck 
 import-module OSD -Force
 
-write-host -ForegroundColor Green "Start OSDCloud"
+write-host -ForegroundColor Green "Modul zaimportowany"
+write-host -ForegroundColor Green "Aby kontynuowac, wybierz 1 w momencie prosby o wybor systemu"
 Start-OSDCloud -FindImageFile -ZTI
 
 wpeutil reboot
